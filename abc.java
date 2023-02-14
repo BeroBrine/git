f096f5d422e99692b8579c4611993a8f38b8d632
BeroBrine <abhishekrana8818@gmail.com>
2023-02-14 17:59:08 +0530

    v2

--- abc.java
+++ abc.java
@@ -5,6 +5,6 @@ public class abc
 {
     public static void main(String args[])
     {
-        System.out.print("this is version 1 of code");
+        System.out.print("this is version 1 of code and its a milestone");
     }
 }
